53c1536d50b0a72077e59e534b376d1a3117309b
linchenlinchen <2595376275@qq.com>
2018-05-21 20:39:23 +0800

    Lab08

---
+++ functions.inc.php
@@ -0,0 +1,81 @@
+<?php
+
+function generateLink($url, $label, $class) {
+   $link = '<a href="' . $url . '" class="' . $class . '">';
+   $link .= $label;
+   $link .= '</a>';
+   return $link;
+}
+
+
+function outputPostRow($number)  {
+    include("travel-data.inc.php");
+//    $resource = 0;
+//    if($number === 1){$resource = "images/8710320515.jpg";}
+//    elseif ($number === 2){$resource = "images/8710289254.jpg";}
+//    else{$resource = "images/8710247776.jpg";}
+    $postId = "postId".$number;
+    $userId = "userId".$number;
+    $userName = "userName".$number;
+    $date = "date".$number;
+    $thumb = "thumb".$number;
+    $title = "title".$number;
+    $excerpt = "excerpt".$number;
+    $reviewsNum = "reviewsNum".$number;
+    $reviewsRating = "reviewsRating".$number;
+    print '<div class="postlist">
+                <div class="row">
+                        <div class="col-md-4">
+                            <a href="post.php?id="'.$$postId . 'class="">'.
+                                '<img src="images/'.$$thumb. '"alt='.$$title.' class="img-responsive"/>
+                            </a>
+                        </div>
+                        <div class="col-md-8">
+                            <h2>'.$$title.'</h2>
+                            <div class="details">Posted by <a href="user.php?id='.$$userId.'" class="">'.$$userName.'</a>
+                                <span class="pull-right">'.$$date.'</span>
+                                <p class="ratings">';
+    if($number === 1) {
+                          print   '<img src="images/star-gold.svg" width="16" /><img src="images/star-gold.svg" width="16" /><img src="images/star-gold.svg" width="16" /><img src="images/star-white.svg" width="16" /><img src="images/star-white.svg" width="16" />';
+        }
+    elseif($number === 2){
+        print                     '<img src="images/star-gold.svg" width="16" /><img src="images/star-gold.svg" width="16" /><img src="images/star-gold.svg" width="16" /><img src="images/star-gold.svg" width="16" /><img src="images/star-gold.svg" width="16" />';
+        }
+        else{
+        print                     '<img src="images/star-gold.svg" width="16" /><img src="images/star-gold.svg" width="16" /><img src="images/star-white.svg" width="16" /><img src="images/star-white.svg" width="16" /><img src="images/star-white.svg" width="16" />';
+        }
+
+
+
+        print $$reviewsNum .' Reviews</p></div><p class="excerpt">
+                            '.$$excerpt.'</p>
+                            <p><a href="post.php?id='.$$userId.'" class="btn btn-primary btn-sm">Read more</a></p>
+                        </div>
+                    </div>
+                    <hr/>
+</div>';
+               //<!-- replace each of these rows with a function call -->
+
+}
+
+/*
+  Function constructs a string containing the <img> tags necessary to display
+  star images that reflect a rating out of 5
+*/
+function constructRating($rating) {
+    $imgTags = "";
+    
+    // first output the gold stars
+    for ($i=0; $i < $rating; $i++) {
+        $imgTags .= '<img src="images/star-gold.svg" width="16" />';
+    }
+    
+    // then fill remainder with white stars
+    for ($i=$rating; $i < 5; $i++) {
+        $imgTags .= '<img src="images/star-white.svg" width="16" />';
+    }    
+    
+    return $imgTags;    
+}
+
+?>
