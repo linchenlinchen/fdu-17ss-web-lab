de7dcc96a2fe551f827d42cbbb9a3b6941f84b06
linchenlinchen <34669038+linchenlinchen@users.noreply.github.com>
2018-07-05 15:21:49 +0800

    Update lab11.php

--- lab11/lab11.php
+++ lab11/lab11.php
@@ -12,19 +12,19 @@
 </nav>
 <section id="s_edit" class="content">
     <form id="f_upload" enctype="multipart/form-data" action = 'forSubmit.php' method = 'post'>
-        <p>请上传音乐文件</p>
+        <p>上传文件</p>
 
         <audio id = "audio"src = "" controls="controls" autoplay="autoplay"></audio><br/>
         <input type="file" name="file_upload" onchange = "document.getElementById('audio').src = window.URL.createObjectURL(this.files[0]); "/>
         <table>
             <tr><td>Title: <input type="text"></td><td>Artist: <input type="text"></td></tr>
             <tr><td colspan="2"><textarea name="edit_lyric" id="edit_lyric"></textarea></td></tr>
-            <tr><td><input type="button" value="插入时间标签" onclick = 'insert(get_target_pos());'></td><td><input type="button" value="替换时间标签" onclick = 'change(get_target_pos())'></td></tr>
+            <tr><td><input type="button" value="插入标签" onclick = 'insert(get_target_pos());'></td>
+                <td><input type="button" value="替换标签" onclick = 'change(get_target_pos())'></td></tr>
             <tr><td colspan="2" id="td_submit"><input type="submit" value="Submit"></td></tr>
         </table>
     </form>
 </section>
-<!--歌词展示部分-->
 <section id="s_show" class="content">
     <select id = "select" onchange = "selectChange();">
         <?php
@@ -32,11 +32,9 @@
         ?>
     </select>
     <audio id = "audio2" src = "" controls="controls" autoplay="autoplay" ontimeupdate = "movelrc();"></audio><br/>
-    <button id = "bt1" onclick = "bt1Click();">上一首</button>
-    <button id = "bt2" onclick = "bt2Click();">下一首</button>
-    <ul id = "lyric2"><div class = "move" id = 'ul2'>
-            <li class = "bold">2333<br/>2333</li><li>2333</li><li>2333</li><li>2333</li>
-        </div></ul>
+    <button id = "bt1" onclick = "bt1Click();">LAST</button>
+    <button id = "bt2" onclick = "bt2Click();">NEXT</button>
+    
     <textarea id="lyric" readonly="true">
     </textarea>
 </section>
